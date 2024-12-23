2ad648e5b0c4390b2a8a3908bd1c439682d32571
sopanha.uonmuon <62048484+sopanha-uonmuon@users.noreply.github.com>
2024-12-23 13:14:59 +0700

    categories

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -9,21 +9,27 @@ const CategoryList = () => {
 			<div className={styles.categories}>
 				<Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
 					<Image src={"/style.png"} alt="" width={32} height={32} className={styles.image}/>
+					Style
 				</Link>
 				<Link href="/blog?cat=style" className={`${styles.category} ${styles.fashion}`}>
 					<Image src={"/fashion.png"} alt="" width={32} height={32} className={styles.image}/>
+					Fashion
 				</Link>
 				<Link href="/blog?cat=style" className={`${styles.category} ${styles.food}`}>
 					<Image src={"/food.png"} alt="" width={32} height={32} className={styles.image}/>
+					Food
 				</Link>
 				<Link href="/blog?cat=style" className={`${styles.category} ${styles.travel}`}>
 					<Image src={"/travel.png"} alt="" width={32} height={32} className={styles.image}/>
+					Travel
 				</Link>
 				<Link href="/blog?cat=style" className={`${styles.category} ${styles.culture}`}>
 					<Image src={"/culture.png"} alt="" width={32} height={32} className={styles.image}/>
+					Culture
 				</Link>
 				<Link href="/blog?cat=style" className={`${styles.category} ${styles.coding}`}>
 					<Image src={"/coding.png"} alt="" width={32} height={32} className={styles.image}/>
+					Coding
 				</Link>
 			</div>
 		</div>
