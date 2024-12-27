f072891f5688ef2a07af9d13133645fc00b39fc0
sopanha-uonmuon <uon.muonsopanha@gmail.com>
2024-12-27 10:50:30 +0700

    backend

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -6,8 +6,6 @@ import Link from 'next/link';
 import { signOut, useSession } from 'next-auth/react';
 const AuthLinks = () => {
 	const [open, setOpen] = useState(false)
-	//temporary
-	// const status = 'notauthenticated';
 	const { status } = useSession();
 	return (
 		<>
